d87e246a2a7b0f1c64de3cfa2a63f2f234a1ccaa
Nathanial Brune <natebrune@protonmail.com>
2023-06-24 22:34:56 -0700

    update manifest.ts

--- aave/manifest.ts
+++ aave/manifest.ts
@@ -1,4 +1,4 @@
-import { Manifest } from "https://deno.land/x/robo_arkiver@v0.4.7/mod.ts";
+import { Manifest } from "https://deno.land/x/robo_arkiver/mod.ts";
 import { hourDataHandler } from "./handlers/hourdata.ts";
 import { AAVEHourData } from "./entities/aavehourdata.ts";
 import { Pool } from "./entities/pool.ts";
@@ -9,7 +9,7 @@ import { Token } from "./entities/token.ts";
 const VelodromeLusdWeth = '0x91e0fC1E4D32cC62C4f9Bc11aCa5f3a159483d31' as const
 const startBlockHeight = 86540000n // aLUSD created on block 86532283
 
-const manifest = new Manifest('aave-hourly-data');
+const manifest = new Manifest('aave-hourly-data-v1');
 const optimism = manifest
 	.addEntities([AAVEHourData, Pool, Token])
 	.chain("optimism")
