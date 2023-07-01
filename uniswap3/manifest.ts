dce3cea74b9ddad1c387f8fce98e5faeb9e82f5d
SmoothBot <smoothbot123@gmail.com>
2023-07-02 01:32:50 +1000

    cleaning up univ3

--- uniswap3/manifest.ts
+++ uniswap3/manifest.ts
@@ -1,19 +1,17 @@
 import { Manifest } from "https://deno.land/x/robo_arkiver/mod.ts";
 import { AmmPool } from "./entities/ammpool.ts";
 import { Token } from "./entities/token.ts";
-import { Swap } from "./entities/swap.ts";
 import { hourDataHandler } from "./handlers/hourdata.ts";
 import { Snapshot } from "./entities/snapshot.ts";
-import { FarmSnapshot } from "./entities/farmsnapshot.ts";
 import { UNI3PoolAbi } from "./abis/UNI3PoolAbi.ts"
 import { onSwap } from "./handlers/swaphandler.ts"
 import { Ohlc } from "./entities/ohlc.ts";
 
 const startBlockHeight = 14353601n 
 //const startBlockHeight = 12440000n
-const manifest = new Manifest('uniswapV3-lucky5');
+const manifest = new Manifest('univ3-ohlc');
 const mainnet = manifest
-	.addEntities([AmmPool, Token, Snapshot, FarmSnapshot, Swap, Ohlc])
+	.addEntities([AmmPool, Token, Snapshot, Ohlc])
 	.chain("mainnet", { blockRange: 500n })
 	
 	
